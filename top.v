77e34dcb02099c8d5f7d93b6bcc9e6d67b2d9b4e
Nitin Mewara <144552858+nitinmewara123@users.noreply.github.com>
2025-07-31 16:47:15 +0530

    Update top.v

--- top.v
+++ top.v
@@ -21,21 +21,22 @@
 
 
 module top(input clk, reset, output memWriteM, output [31:0] ALUResultM, writeDataM, readDataM, pcF, InstrF  );
-wire FlushE,StallD,FlushD,StallF, ZeroE, ALUSrcE, PCSrcE, RegWriteM, RegWriteW ;
+wire FlushE,StallD,FlushD,StallF, branch_taken, ALUSrcE, PCSrcE, RegWriteM, RegWriteW ;
 wire [1:0] ImmSrcD, ResultSrcE,ResultSrcW, ForwardAE, ForwardBE;
 wire [2:0] ALUControlE;
 wire [31:0] InstrD, PCD, PCPlus4D, PCTargetE, PCPlus4M; 
 wire [4:0] RS1E, RS2E,RDE, RDW, RDM ;
-
-    
-Controller controller1(clk, reset, FlushE,ZeroE, InstrD, ImmSrcD, ALUSrcE,
-PCSrcE, ResultSrcE, ALUControlE,memWriteM,RegWriteM, RegWriteW, ResultSrcW);
+wire [2:0] functE, functM; 
 
 
-data_Path dataPath(clk,reset,InstrD,ImmSrcD,PCD,PCPlus4D,RS1E,ResultSrcW,readDataM,
+Controller controller1(clk, reset, FlushE,branch_taken, InstrD, ImmSrcD, ALUSrcE,
+PCSrcE, ResultSrcE, ALUControlE,memWriteM,RegWriteM, RegWriteW, ResultSrcW, functE, functM);
+
+
+data_Path dataPath(clk,reset,InstrD,ImmSrcD,PCD,PCPlus4D,RS1E,functE,functM, ResultSrcW,readDataM,
 RS2E, RDE,RegWriteW,FlushE,ForwardAE,ForwardBE,
 ALUControlE,ALUSrcE, RDM,PCPlus4M, writeDataM, ALUResultM,RDW,
-PCTargetE,ZeroE);
+PCTargetE,branch_taken);
 
 Hazard hazard_unit(RegWriteW, RDW, RegWriteM, RDM, ResultSrcE, ForwardBE, ForwardAE, PCSrcE,
 RS1E, RS2E, RDE, FlushE, InstrD[19:15], InstrD[24:20] , StallD, FlushD, StallF );
@@ -48,5 +49,6 @@ Data_Memory dmem(clk, reset, memWriteM,
     
 imem imem1(pcF, InstrF); 
 endmodule
+
 
 
