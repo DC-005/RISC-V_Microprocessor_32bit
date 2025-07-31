52d7e010245f334cab34d9d6e7fe4d2a466340c4
Nitin Mewara <144552858+nitinmewara123@users.noreply.github.com>
2025-07-31 16:31:52 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,8 +1,9 @@
 `timescale 1ns/1ps
 module ALU(
     input [31:0] SrcAE, SrcBE,       
-    input [2:0] ALUControlE,   
-    output reg ZeroE,          
+    input [2:0] ALUControlE,
+    input [2:0] functE,   
+    output reg branch_taken,          
     output reg [31:0] ALUResult  
 );
 
@@ -24,9 +25,19 @@ module ALU(
             3'b111: ALUResult = SrcAE >>> 1;         
             default: ALUResult = 32'hxxxxxxxx;             
         endcase
-        
-        ZeroE = (ALUResult == 32'b0);
-    end
+        end
+     always @(*) begin
+        case (functE)
+            3'b000: branch_taken = (SrcAE == SrcBE);                  // beq
+            3'b001: branch_taken = (SrcAE != SrcBE);                  // bne
+            3'b100: branch_taken = ($signed(SrcAE) < $signed(SrcBE)); // blt
+            3'b101: branch_taken = ($signed(SrcAE) >= $signed(SrcBE)); // bge
+            3'b110: branch_taken = (SrcAE < SrcBE);                   // bltu
+            3'b111: branch_taken = (SrcAE >= SrcBE);                  // bgeu
+            default: branch_taken = 1'b0;
+        endcase
+        end
 
 endmodule
+
 
